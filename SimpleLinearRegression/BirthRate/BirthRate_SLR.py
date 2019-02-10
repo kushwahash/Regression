de55a533c2ff9ef9c43b4cc71f2561f0a5e69652
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-02-09 20:17:03 -0600

    1. Read Data. 2. Split test and training set. 3. Visualize actual data.

--- SimpleLinearRegression/BirthRate/BirthRate_SLR.py
+++ SimpleLinearRegression/BirthRate/BirthRate_SLR.py
@@ -10,3 +10,16 @@ dataset = pd.read_csv("BirthvsPoverty.csv")
 X = dataset.iloc[:, 1].values
 #Using Third Column,Brth15to17 as Dependent variable. 
 y = dataset.iloc[:, 2].values
+
+
+#split the dataset into training set and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=1/5,random_state=0)
+
+#Visualize (show) the actual data set. We can take a look and see if using SLR is a good idea (Slope)
+plt.scatter(X,y,color = 'red')
+#add a Title to better understand
+plt.title('Poverty vs Teen Birth (Age15to17) Training Set (Actual Data)')
+plt.xlabel('Poverty')
+plt.ylabel('Teen Birth')
+plt.show()
