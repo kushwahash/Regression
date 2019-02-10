a5e7250ced8a51cc337b7c6e4662a114cf805314
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-02-09 21:30:43 -0600

    Added visualization.

--- SimpleLinearRegression/BirthRate/BirthRate_SLR.py
+++ SimpleLinearRegression/BirthRate/BirthRate_SLR.py
@@ -37,6 +37,23 @@ print("++++ Predicting Test Data +++++")
 for i in range(0,len(y_test)):
     print("Actual :: {}, Predicted :: {:.1f}".format(y_train[i],y_pred_test[i]))
 
-#visualize the data and the predicted line
+#visualize the data and the predicted line train data
+plt.scatter(X_train,y_train,color = 'red')
+#visualize the Train this will show the line which is predicted
+plt.plot(X_train,slr_birth.predict(X_train),color = 'blue')
+#add a Title to better understand
+plt.title('Povert vs Teen Birth Rate (Age15to17) Training Data Set')
+plt.xlabel('Poverty')
+plt.ylabel('Teen Birth Rate')
+plt.show()
 
+#visualize the data and the predicted line test data
+plt.scatter(X_test,y_test,color = 'red')
+#visualize the Train this will show the line which is predicted
+plt.plot(X_train,slr_birth.predict(X_train),color = 'blue')
+#add a Title to better understand
+plt.title('Povert vs Teen Birth Rate (Age15to17) Test Data Set')
+plt.xlabel('Poverty')
+plt.ylabel('Teen Birth Rate')
+plt.show()
 
