c8ee3e58bc3744da49bbe4b17f567042be484c17
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-02-09 20:06:43 -0600

    Updated data file name.

--- SimpleLinearRegression/BirthRate/BirthRate_SLR.py
+++ SimpleLinearRegression/BirthRate/BirthRate_SLR.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #read the data file
-dataset = pd.read_csv("Birth_Rate.csv")
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+dataset = pd.read_csv("BirthvsPoverty.csv")
+#Will take Second Column Poverty Percentage as Independent variable.
+X = dataset.iloc[:, 1].values
+#Using Third Column,Brth15to17 as Dependent variable. 
+y = dataset.iloc[:, 2].values
