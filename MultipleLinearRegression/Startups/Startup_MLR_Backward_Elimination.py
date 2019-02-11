b91445715b12726bedd466c47fe4758a1bad60b0
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-02-10 19:58:13 -0600

    Adding backward elimination for MLR.

---
+++ MultipleLinearRegression/Startups/Startup_MLR_Backward_Elimination.py
@@ -0,0 +1,56 @@
+'''
+Using Multiple Linear Regression by backward Elimination#
+This script will have additional steps for backward elimination to chose the best independent variables.
+By best, we mean the one which have more impact in predicting the output.
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import utility.utils as utl
+#read the file
+dataset = pd.read_csv("50_Startups.csv")
+#set the independent and dependent variable
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 4].values
+
+
+#Encoding categorical data (State)
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelEnc_X = LabelEncoder()
+#country is at index 3
+X[:,3] = labelEnc_X.fit_transform(X[:,3])
+#you might want to check for categories='auto'
+onehotEnc = OneHotEncoder(categorical_features=[3])
+X = onehotEnc.fit_transform(X).toarray()
+
+#Avoid the dummy variable Trap, remove one dummy variable column
+#Selecting all column starting from index - 1, In most cases libraries take care of it
+X = X[:,1:]
+
+
+#divide X,Y into test and training set
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=1/5,random_state=0)
+
+#fitting the data into MLR
+
+from sklearn.linear_model import LinearRegression
+startup_mlr = LinearRegression()
+
+startup_mlr.fit(X_train,y_train)
+
+#model created, lets test the model on test data
+y_pred_test = startup_mlr.predict(X_test)
+
+utl.print_actual_predicted(y_test,y_pred_test,True)
+
+#model created, lets test the model on training data
+y_pred_train = startup_mlr.predict(X_train)
+
+utl.print_actual_predicted(y_train,y_pred_train,True)
+
+
+
+
+
